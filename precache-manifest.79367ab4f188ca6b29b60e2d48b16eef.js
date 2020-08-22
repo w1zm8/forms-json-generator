6e48442cdb955f1f466aa08a024ee9b5cc34e5b2
w1zm8 <w1zm8@hey.com>
2020-08-23 02:59:14 +0300

    Updates

---
+++ precache-manifest.79367ab4f188ca6b29b60e2d48b16eef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea36f58807dac0b61a6cce34b625600",
+    "url": "/forms-json-generator/index.html"
+  },
+  {
+    "revision": "4d285f1d7dd794b56d8f",
+    "url": "/forms-json-generator/static/css/main.9099e782.chunk.css"
+  },
+  {
+    "revision": "9853f04ab879bb39c6c4",
+    "url": "/forms-json-generator/static/js/2.e0f0efeb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/forms-json-generator/static/js/2.e0f0efeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d285f1d7dd794b56d8f",
+    "url": "/forms-json-generator/static/js/main.6ca7061a.chunk.js"
+  },
+  {
+    "revision": "40d6ef85566b5b166d45",
+    "url": "/forms-json-generator/static/js/runtime-main.df3a38f9.js"
+  }
+]);
