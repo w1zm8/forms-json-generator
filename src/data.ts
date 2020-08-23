e0b34c2afa3f8a50ca01fc37992790d6a5bca1e6
w1zm8 <w1zm8@hey.com>
2020-08-23 03:31:20 +0300

    changed format of form templates to string

--- src/data.ts
+++ src/data.ts
@@ -1,147 +1,149 @@
-export const signInFormData = JSON.stringify({
-  items: [
+export const signInFormData = `{
+  "items": [
     {
-      type: "string",
-      label: "Username",
-      props: {
-        id: "username",
-        placeholder: "Enter username",
-      },
+      "type": "string",
+      "label": "Username",
+      "props": {
+        "id": "username",
+        "placeholder": "Enter username"
+      }
     },
     {
-      type: "string",
-      label: "Password",
-      props: {
-        type: "password",
-        id: "password",
-        placeholder: "Enter password",
-      },
-    },
+      "type": "string",
+      "label": "Password",
+      "props": {
+        "type": "password",
+        "id": "password",
+        "placeholder": "Enter password"
+      }
+    }
   ],
-  controls: [
+  "controls": [
     {
-      label: "Forgot password",
-      props: {
-        variant: "light",
-      },
+      "label": "Forgot password",
+      "props": {
+        "variant": "light"
+      }
     },
     {
-      label: "Sign In",
-      props: {
-        variant: "primary",
-      },
-    },
-  ],
-});
+      "label": "Sign In",
+      "props": {
+        "variant": "primary"
+      }
+    }
+  ]
+}`;
 
-export const signUpFormData = JSON.stringify({
-  items: [
+export const signUpFormData = `{
+  "items": [
     {
-      type: "string",
-      label: "Email",
-      props: {
-        id: "email",
-        placeholder: "Enter email",
-        defaultValue: "somenewuser@gmail.com",
-      },
+      "type": "string",
+      "label": "Email",
+      "props": {
+        "id": "email",
+        "placeholder": "Enter email",
+        "defaultValue": "somenewuser@gmail.com"
+      }
     },
     {
-      type: "string",
-      label: "Username",
-      props: {
-        id: "username",
-        placeholder: "Enter username",
-        defaultValue: "somenewuser",
-      },
+      "type": "string",
+      "label": "Username",
+      "props": {
+        "id": "username",
+        "placeholder": "Enter username",
+        "defaultValue": "somenewuser"
+      }
     },
     {
-      type: "string",
-      label: "Password",
-      props: {
-        type: "password",
-        id: "password",
-        placeholder: "Enter password",
-      },
+      "type": "string",
+      "label": "Password",
+      "props": {
+        "type": "password",
+        "id": "password",
+        "placeholder": "Enter password"
+      }
     },
     {
-      type: "string",
-      label: "Repeat password",
-      props: {
-        type: "password",
-        id: "repeatPassword",
-        placeholder: "Repeat password",
-      },
+      "type": "string",
+      "label": "Repeat password",
+      "props": {
+        "type": "password",
+        "id": "repeatPassword",
+        "placeholder": "Repeat password"
+      }
     },
     {
-      type: "boolean",
-      label: "I accept the Terms of Service",
-      props: {
-        id: "agreement",
-      },
-    },
+      "type": "boolean",
+      "label": "I accept the Terms of Service",
+      "props": {
+        "id": "agreement"
+      }
+    }
   ],
-  controls: [
+  "controls": [
     {
-      label: "Create account",
-      props: {
-        variant: "primary",
-      },
-    },
-  ],
-});
+      "label": "Create account",
+      "props": {
+        "variant": "primary"
+      }
+    }
+  ]
+}
+`;
 
-export const createPostFormData = JSON.stringify({
-  title: "Create new post",
-  items: [
+export const createPostFormData = `{
+  "title": "Create new post",
+  "items": [
     {
-      label: "Title",
-      type: "string",
-      props: {
-        id: "title",
-        placeholder: "Enter post title",
-      },
+      "label": "Title",
+      "type": "string",
+      "props": {
+        "id": "title",
+        "placeholder": "Enter post title"
+      }
     },
     {
-      label: "Description",
-      type: "text",
-      props: {
-        id: "description",
-        placeholder: "Enter post description",
-      },
+      "label": "Description",
+      "type": "text",
+      "props": {
+        "id": "description",
+        "placeholder": "Enter post description"
+      }
     },
     {
-      label: "Content",
-      type: "text",
-      props: {
-        id: "content",
-        placeholder: "Enter post contet",
-        rows: 5,
-      },
+      "label": "Content",
+      "type": "text",
+      "props": {
+        "id": "content",
+        "placeholder": "Enter post contet",
+        "rows": 5
+      }
     },
     {
-      label: "Public",
-      type: "radio",
-      props: {
-        id: "radio1",
-        name: "access",
-        checked: true,
-      },
+      "label": "Public",
+      "type": "radio",
+      "props": {
+        "id": "radio1",
+        "name": "access",
+        "checked": true
+      }
     },
     {
-      label: "Private",
-      type: "radio",
-      props: {
-        id: "radio2",
-        name: "access",
-      },
-    },
+      "label": "Private",
+      "type": "radio",
+      "props": {
+        "id": "radio2",
+        "name": "access"
+      }
+    }
   ],
-  controls: [
+  "controls": [
     {
-      label: "Create post",
-      props: {
-        variant: "primary",
-      },
-    },
-  ],
-});
+      "label": "Create post",
+      "props": {
+        "variant": "primary"
+      }
+    }
+  ]
+}
+`;
